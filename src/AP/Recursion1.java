7a08490c00ae023788ec6ed26f0601d5777606f5
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-02-12 20:38:41 +0100

    update

--- src/AP/Recursion1.java
+++ src/AP/Recursion1.java
@@ -1,14 +1,31 @@
 package AP;
 
-import static java.lang.Integer.sum;
-
 public class Recursion1 {
     public int factorial(int n) {
         if (n <= 1) {
             return 1;
-        }
-        else {
-            return n * factorial(n- 1);
+        } else {
+            return n * factorial(n - 1);
         }
     }
+
+    public int bunnyEars(int bunnies) {
+        if (bunnies < 1) {
+            return 0;
+        } else {
+            return bunnies * 2;
+        }
+    }
+
+    public int fibonacci(int n) {
+        if (n == 0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
 }
