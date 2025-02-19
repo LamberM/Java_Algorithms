7c5419736d88badfd33655cf8b5224fe8d4540bf
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-02-19 18:02:02 +0100

    update

--- src/AP/Functional2.java
+++ src/AP/Functional2.java
@@ -9,5 +9,9 @@ public class Functional2 {
                 .filter(integer -> integer >= 0)
                 .collect(Collectors.toList());
     }
-
+    public List<Integer> no9(List<Integer> nums) {
+        return nums.stream()
+                .filter(integer -> integer == integer % 9)
+                .collect(Collectors.toList());
+    }
 }
