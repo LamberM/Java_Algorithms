872b801a8748cd04f2e28a734d72559abba26222
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-02-23 15:23:34 +0100

    update

--- src/AP/Functional2.java
+++ src/AP/Functional2.java
@@ -9,20 +9,43 @@ public class Functional2 {
                 .filter(integer -> integer >= 0)
                 .collect(Collectors.toList());
     }
+
     public List<Integer> no9(List<Integer> nums) {
         return nums.stream()
                 .filter(integer -> integer == integer % 9)
                 .collect(Collectors.toList());
     }
+
     public List<Integer> noTeen(List<Integer> nums) {
         return nums.stream()
                 .filter(integer -> integer < 13 || integer > 19)
                 .collect(Collectors.toList());
     }
+
     public List<String> noZ(List<String> strings) {
         return strings.stream()
                 .filter(s -> !s.contains("z"))
                 .collect(Collectors.toList());
+    }
+
+    public List<String> noLong(List<String> strings) {
+        return strings.stream()
+                .filter(s -> s.length() < 4)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> no34(List<String> strings) {
+        return strings.stream()
+                .filter(s -> s.length() != 3 && s.length() != 4)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> noYY(List<String> strings) {
+        List<String> newStrings = strings.stream()
+                .filter(s -> !s.endsWith("y") && !s.contains("yy"))
+                .collect(Collectors.toList());
+        newStrings.replaceAll(s-> s+"y");
+        return newStrings;
     }
 
 }
