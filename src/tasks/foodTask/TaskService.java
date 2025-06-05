fc168b4b8b5c07d64f9fe85a3a74f8d5aed0f0af
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-06-05 19:48:57 +0200

    update

--- src/tasks/foodTask/TaskService.java
+++ src/tasks/foodTask/TaskService.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class taskService {
+public class TaskService {
     public int calculateAllKcal() {
         var result = 0;
         List<Recipe> recipeList = createExample();
